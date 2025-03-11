00ec3b92342adbd210e663b37b85fcefa2dd9d69
Gabriel Mata <gabrielmatau@gmail.com>
2025-03-11 16:28:20 -0500

    feat: add postgres Message Pickup Repository package (#47)
    Signed-off-by: Gabriel Mata <gabrielmatau@gmail.com>

---
+++ packages/message-pickup-repository-pg/src/interfaces.ts
@@ -0,0 +1,28 @@
+import { EncryptedMessage, Logger } from '@credo-ts/core'
+import { MessagePickupSession } from '@credo-ts/core/build/modules/message-pickup/MessagePickupSession'
+
+export interface PostgresMessagePickupRepositoryConfig {
+  logger?: Logger
+  postgresUser: string
+  postgresPassword: string
+  postgresHost: string
+  postgresDatabaseName?: string
+}
+
+export const MessageQueuedEventType: string = 'MessagePickupRepositoryMessageQueued'
+
+export interface MessageQueuedEvent {
+  message: {
+    id: string
+    connectionId: string
+    recipientDids: string[]
+    encryptedMessage: EncryptedMessage
+    receivedAt: Date
+    state: string
+  }
+  session?: MessagePickupSession
+}
+
+export interface ExtendedMessagePickupSession extends MessagePickupSession {
+  isLocalSession: boolean
+}
