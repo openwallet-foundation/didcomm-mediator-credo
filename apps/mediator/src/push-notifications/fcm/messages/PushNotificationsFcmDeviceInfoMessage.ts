4ae623c80952d1a0996d79a495a4b967f1c26eba
jamshale <31809382+jamshale@users.noreply.github.com>
2025-09-15 10:42:14 -0700

    Credo upgrade (#64)
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- apps/mediator/src/push-notifications/fcm/messages/PushNotificationsFcmDeviceInfoMessage.ts
+++ apps/mediator/src/push-notifications/fcm/messages/PushNotificationsFcmDeviceInfoMessage.ts
@@ -37,7 +37,8 @@ export class PushNotificationsFcmDeviceInfoMessage extends AgentMessage {
   @ValidateIf((_, value) => value !== null)
   public devicePlatform!: string | null
 
+  public static readonly type = parseMessageType('https://didcomm.org/push-notifications-fcm/1.0/device-info')
+
   @IsValidMessageType(PushNotificationsFcmDeviceInfoMessage.type)
   public readonly type = PushNotificationsFcmDeviceInfoMessage.type.messageTypeUri
-  public static readonly type = parseMessageType('https://didcomm.org/push-notifications-fcm/1.0/device-info')
 }
