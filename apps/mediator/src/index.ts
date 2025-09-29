260bf3142da4e6fbe9e5de47eafa829ef0058e39
jamshale <31809382+jamshale@users.noreply.github.com>
2025-09-28 23:58:38 -0700

    feat: Goal code and new invitation configurations (#65)

--- apps/mediator/src/index.ts
+++ apps/mediator/src/index.ts
@@ -1,32 +1,55 @@
-import { OutOfBandRepository, OutOfBandRole, OutOfBandState } from '@credo-ts/core'
+import { Agent, OutOfBandRecord, OutOfBandRepository, OutOfBandRole, OutOfBandState } from '@credo-ts/core'
 
 import { createAgent } from './agent'
 import config from './config'
 
+function logInvitationUrl(agent: Agent, outOfBandRecord: OutOfBandRecord) {
+  const httpEndpoint = agent.config.endpoints.find((e) => e.startsWith('http'))
+  if (!httpEndpoint) {
+    throw new Error('No HTTP endpoint configured for invitation generation')
+  }
+
+  const invitationEndpoint = config.get('agent:invitationUrl') ?? `${httpEndpoint}/invite`
+  const mediatorInvitationUrlLong = outOfBandRecord.outOfBandInvitation.toUrl({
+    domain: invitationEndpoint,
+  })
+
+  agent.config.logger.info(`Out of band invitation url:\n\n\t${mediatorInvitationUrlLong}`)
+}
+
+async function createMediatorInvitation(agent: Agent) {
+  return agent.oob.createInvitation({
+    multiUseInvitation: true,
+    goalCode: config.get('agent:goalCode'),
+    goal: 'Mediator Invitation',
+  })
+}
+
 void createAgent().then(async (agent) => {
   agent.config.logger.info('Agent started')
 
-  // Try to find existing out of band record
+  if (config.get('agent:recreateInvitation')) {
+    agent.config.logger.info('Recreating out of band invitation')
+    const outOfBandRecord = await createMediatorInvitation(agent)
+    return logInvitationUrl(agent, outOfBandRecord)
+  }
+
   const oobRepo = agent.dependencyManager.resolve(OutOfBandRepository)
   const outOfBandRecords = await oobRepo.findByQuery(agent.context, {
     state: OutOfBandState.AwaitResponse,
     role: OutOfBandRole.Sender,
   })
 
-  let outOfBandRecord = outOfBandRecords.find((oobRecord) => oobRecord.reusable)
+  let outOfBandRecord = outOfBandRecords
+    .filter((oobRecord) => oobRecord.reusable)
+    .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())[0]
 
-  // If it does't exist, we create a new one
-  if (!outOfBandRecord) {
-    outOfBandRecord = await agent.oob.createInvitation({
-      multiUseInvitation: true,
-    })
+  if (outOfBandRecord) {
+    agent.config.logger.info('Reusing existing out of band invitation')
+  } else {
+    agent.config.logger.warn('No reusable out of band invitation found, creating a new one')
+    outOfBandRecord = await createMediatorInvitation(agent)
   }
 
-  const httpEndpoint = agent.config.endpoints.find((e) => e.startsWith('http')) as string
-  const invitationEndpoint = config.get('agent:invitationUrl') ?? `${httpEndpoint}/invite`
-  const mediatorInvitationUrlLong = outOfBandRecord.outOfBandInvitation.toUrl({
-    domain: invitationEndpoint,
-  })
-
-  agent.config.logger.info(`Out of band invitation url: \n\n\t${mediatorInvitationUrlLong}`)
+  logInvitationUrl(agent, outOfBandRecord)
 })
