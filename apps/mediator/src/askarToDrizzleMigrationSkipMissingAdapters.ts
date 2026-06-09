ba705de4983625bb9a30d213f65787436460481d
devdgna <dev.dgna@gmail.com>
2026-06-09 14:09:36 +0600

    fix: Omit migrating old queued messages which cannot be delivered
    Signed-off-by: devdgna <dev.dgna@gmail.com>

---
+++ apps/mediator/src/askarToDrizzleMigrationSkipMissingAdapters.ts
@@ -0,0 +1,28 @@
+import { AskarToDrizzleStorageMigrator } from '@credo-ts/askar-to-drizzle-storage-migration'
+import { agentDependencies } from '@credo-ts/node'
+import { loadAskar } from './config/askarLoader.js'
+import { loadStorage } from './config/storageLoader.js'
+import { logger } from './config.js'
+
+export async function askarToDrizzleMigrationSkipMissingAdapters() {
+  const { askar: askarModule } = await loadAskar({ enableStorage: true })
+  if (!askarModule) throw new Error('Expected Askar to be configured')
+
+  const { drizzle: drizzleModule } = loadStorage()
+  if (!drizzleModule) throw new Error('Expected Drizzle to be configured')
+
+  const migrator = await AskarToDrizzleStorageMigrator.initialize({
+    drizzleModule,
+    agentDependencies,
+    logger,
+    askarModule,
+    skipMigrationForMissingAdapter: true,
+  })
+
+  await migrator.migrate()
+}
+
+askarToDrizzleMigrationSkipMissingAdapters().then(() => {
+  logger.info('Migration complete')
+  process.exit()
+})
